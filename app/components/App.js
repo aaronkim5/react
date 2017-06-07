2a07f6adeeac8a85840fd4934715e3b70c57c12e
Aaron Kim <aaronkim1205@gmail.com>
2017-06-06 18:25:13 -0700

    npm install react router dom

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,7 @@
 var React = require('react');
 var Popular = require('./Popular')
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
 
 class App extends React.Component {
   render () {
